1167f94fa90ed4c13443a402a028ce7f28decd5f
justin550vx <justin.abrams@hotmail.com>
2021-11-05 15:58:52 +0200

    Added lose and waiting penalty

--- src/dqn.py
+++ src/dqn.py
@@ -122,6 +122,7 @@ class DQNAgent:
         batch_size,
         gamma,
         device=torch.device("cuda"),
+        model_dir=None,
     ):
         """
         Initialise the DQN algorithm using the Adam optimiser
@@ -132,16 +133,21 @@ class DQNAgent:
         :param batch_size: the batch size
         :param gamma: the discount factor
         """
-        self.batch_size = batch_size
-        self.gamma = gamma
-        self.memory = replay_buffer
-        self.use_double_dqn = use_double_dqn
-        self.target_network = DQN(input_size, n_actions).to(device)
-        self.policy_network = DQN(input_size, n_actions).to(device)
-        self.update_target_network()
-        self.target_network.eval()
-        self.optimiser = torch.optim.RMSprop(self.policy_network.parameters(), lr=lr)        
-        self.device = device
+        if (model_dir==None):
+            self.batch_size = batch_size
+            self.gamma = gamma
+            self.memory = replay_buffer
+            self.use_double_dqn = use_double_dqn
+            self.target_network = DQN(input_size, n_actions).to(device)
+            self.policy_network = DQN(input_size, n_actions).to(device)
+            self.update_target_network()
+            self.target_network.eval()
+            self.optimiser = torch.optim.RMSprop(self.policy_network.parameters(), lr=lr)        
+            self.device = device
+        else:
+            self.policy_network = torch.load(model_dir)    
+            self.device = device
+
 
     def optimise_td_loss(self):
         """
@@ -203,7 +209,7 @@ class DQNAgent:
             return action.item()
 
 #===================================================================================================
-#=================================================Train=============================================
+#==============================================Training=============================================
 #===================================================================================================
 
 import random
@@ -212,80 +218,10 @@ import gym
 import minihack
 from nle import nethack
 import argparse
+import time
 import torch
 
-if __name__ == "__main__":
-
-    hyper_params = {
-        "seed": 42,  # which seed to use
-        "env": "PongNoFrameskip-v4",  # name of the game
-        "replay-buffer-size": int(5e3),  # replay buffer size
-        "learning-rate": 1e-4,  # learning rate for Adam optimizer
-        "discount-factor": 0.99,  # discount factor
-        "num-steps": int(1e7),  # total number of steps to run the environment for
-        "batch-size": 256,  # number of transitions to optimize at the same time
-        "learning-starts": 10000,  # number of steps before learning starts
-        "learning-freq": 5,  # number of iterations between every optimization step
-        "use-double-dqn": True,  # use double deep Q-learning
-        "target-update-freq": 1000,  # number of iterations between every target network update
-        "eps-start": 1.0,  # e-greedy start threshold
-        "eps-end": 0.01,  # e-greedy end threshold
-        "eps-fraction": 0.1,  # fraction of num-steps
-        "print-freq": 25, # number of iterations between each print out
-        "save-freq": 500, # number of iterations between each model save
-    }
-
-    np.random.seed(hyper_params["seed"])
-    random.seed(hyper_params["seed"])
-
-    # ACTIONS define the actions allowed by the agent
-    MOVE_ACTIONS = tuple(nethack.CompassDirection)
-    NAVIGATE_ACTIONS = MOVE_ACTIONS + (
-        nethack.Command.OPEN,   # Not sure if needed
-        nethack.Command.KICK,   # Not sure if needed
-        nethack.Command.SEARCH, # Not sure if needed
-        nethack.Command.JUMP,   # Not sure if needed
-        nethack.Command.LOOK,   # Not sure if needed
-        nethack.Command.LOOT,   # Not sure if needed
-        nethack.Command.PICKUP, # Not sure if needed
-        nethack.Command.PRAY,   # Not sure if needed
-        nethack.Command.WEAR,   # Not sure if needed
-        nethack.Command.WIELD,  # Not sure if needed
-        nethack.Command.UNTRAP, # Not sure if needed
-        
-        # Might need more? All actions and descriptions found here
-        # https://minihack.readthedocs.io/en/latest/getting-started/action_spaces.html
-    )
-
-    # Create env with modified actions
-    # Probably can limit the observations as well
-    env = gym.make(
-        "MiniHack-Quest-Easy-v0",
-        actions=NAVIGATE_ACTIONS
-    )
-    env.seed(hyper_params["seed"])
-
-    # Monitor for gif
-    # env = gym.wrappers.Monitor(
-    #     env, './video/', video_callable=lambda episode_id: episode_id % 50 == 0, force=True)
-
-    replay_buffer = ReplayBuffer(hyper_params["replay-buffer-size"])
-
-    # Glyphs are 21 by 79
-    input_size = 1659 # 21 * 79
-
-    agent = DQNAgent(
-        input_size,
-        env.action_space.n,
-        replay_buffer,
-        use_double_dqn=hyper_params["use-double-dqn"],
-        lr=hyper_params['learning-rate'],
-        batch_size=hyper_params['batch-size'],
-        gamma=hyper_params['discount-factor'],
-        device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-    )
-
-
+def train(env, agent):
     eps_timesteps = hyper_params["eps-fraction"] * float(hyper_params["num-steps"])
     e_rewards = [0.0]
     s = env.reset()['glyphs']
@@ -346,3 +282,157 @@ if __name__ == "__main__":
             and len(e_rewards) % hyper_params["save-freq"] == 0
         ):
             agent.save("model" + str(len(e_rewards)))
+
+
+def test(env, agent):
+    s = env.reset()['glyphs']
+    s = s.flatten()
+    episode_count = 0
+    reward_for_episode = 0
+    prev_reward = 0
+    for t in range(hyper_params["num-steps"]):
+
+        time.sleep(0.2)
+        env.render()
+        a = agent.act(s)
+
+        print("Episode:", str(episode_count), "\tTime step:", str(t),  "\tAction Taken:", str(a))
+        print("Current Episode Reward:", str(reward_for_episode))
+        print("Previous Episode Reward:", str(prev_reward))
+
+        ns, reward, done, info = env.step(a)
+        reward_for_episode += reward
+        ns = ns['glyphs'].flatten()
+        s = ns
+
+        if done:
+            s = env.reset()['glyphs']
+            s = s.flatten()
+            episode_count += 1
+            prev_reward = reward_for_episode
+            reward_for_episode = 0
+
+
+def create_env():
+    global hyper_params
+    # ACTIONS define the actions allowed by the agent
+    MOVE_ACTIONS = tuple(nethack.CompassDirection)
+    NAVIGATE_ACTIONS = MOVE_ACTIONS + (
+        nethack.Command.OPEN,   # Not sure if needed
+        nethack.Command.KICK,   # Not sure if needed
+        nethack.Command.SEARCH, # Not sure if needed
+        nethack.Command.JUMP,   # Not sure if needed
+        nethack.Command.LOOK,   # Not sure if needed
+        nethack.Command.LOOT,   # Not sure if needed
+        nethack.Command.PICKUP, # Not sure if needed
+        nethack.Command.PRAY,   # Not sure if needed
+        nethack.Command.WEAR,   # Not sure if needed
+        nethack.Command.WIELD,  # Not sure if needed
+        nethack.Command.UNTRAP, # Not sure if needed
+
+        # Might need more? All actions and descriptions found here
+        # https://minihack.readthedocs.io/en/latest/getting-started/action_spaces.html
+    )
+
+    # Create env with modified actions
+    # Probably can limit the observations as well
+    env = gym.make(
+        hyper_params["env-name"],
+        actions=NAVIGATE_ACTIONS,
+        reward_lose=-10,
+        penalty_time=-0.001
+    )
+    env.seed(hyper_params["seed"])
+    return env
+
+
+def create_agent(input_size=1659, n_actions=12, testing_only=False, model_dir=""):
+    global hyper_params
+    if testing_only:
+        if (model_dir==""): 
+            print("No model dir given")
+            exit(1)
+
+        agent = DQNAgent(
+            None,
+            None,
+            None,
+            use_double_dqn=None,
+            lr=None,
+            batch_size=None,
+            gamma=None,
+            device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+            model_dir=model_dir
+        )
+        return agent
+    else:
+        agent = DQNAgent(
+            input_size,
+            n_actions,
+            replay_buffer,
+            use_double_dqn=hyper_params["use-double-dqn"],
+            lr=hyper_params['learning-rate'],
+            batch_size=hyper_params['batch-size'],
+            gamma=hyper_params['discount-factor'],
+            device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        )
+        return agent
+
+
+if __name__ == "__main__":
+
+    my_parser = argparse.ArgumentParser(description='MiniHack RL project')
+
+    # Add the arguments
+    my_parser.add_argument('-test',
+                       type=str,
+                       help='test a model')
+
+    # Execute the parse_args() method
+    args = my_parser.parse_args()
+
+    hyper_params = {
+        "seed": 42,  # which seed to use
+        "env-name": "MiniHack-Quest-Easy-v0",  # name of the game
+        "replay-buffer-size": int(5e3),  # replay buffer size
+        "learning-rate": 1e-4,  # learning rate for Adam optimizer
+        "discount-factor": 0.99,  # discount factor
+        "num-steps": int(5e6),  # total number of steps to run the environment for
+        "batch-size": 256,  # number of transitions to optimize at the same time
+        "learning-starts": 10000,  # number of steps before learning starts
+        "learning-freq": 2,  # number of iterations between every optimization step
+        "use-double-dqn": True,  # use double deep Q-learning
+        "target-update-freq": 1000,  # number of iterations between every target network update
+        "eps-start": 1.0,  # e-greedy start threshold
+        "eps-end": 0.01,  # e-greedy end threshold
+        "eps-fraction": 0.1,  # fraction of num-steps
+        "print-freq": 25, # number of iterations between each print out
+        "save-freq": 500, # number of iterations between each model save
+    }
+
+    np.random.seed(hyper_params["seed"])
+    random.seed(hyper_params["seed"])
+
+    # Monitor for gif
+    # env = gym.wrappers.Monitor(
+    #     env, './video/', video_callable=lambda episode_id: episode_id % 50 == 0, force=True)
+
+    replay_buffer = ReplayBuffer(hyper_params["replay-buffer-size"])
+
+    # Glyphs are 21 by 79
+    input_size = 1659 # 21 * 79
+
+    if args.test:
+        env = create_env()
+        agent = create_agent(testing_only=True, model_dir=args.test)
+        test(env, agent)
+
+    else:
+        env = create_env()
+        agent = create_agent(input_size, env.action_space.n)
+        train(env, agent)
+
+    
+
+
+    
