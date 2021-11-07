54496f8203ec6debb8a7294c81768e93d5d63ecc
justin550vx <justin.abrams@hotmail.com>
2021-11-07 10:25:44 +0200

    Num-steps increase and more random

--- src/dqn.py
+++ src/dqn.py
@@ -339,8 +339,8 @@ def create_env():
     env = gym.make(
         hyper_params["env-name"],
         actions=NAVIGATE_ACTIONS,
-        reward_lose=-10,
-        penalty_time=-0.001
+        reward_lose=-2,
+        reward_win=2
     )
     env.seed(hyper_params["seed"])
     return env
@@ -393,7 +393,7 @@ if __name__ == "__main__":
 
     hyper_params = {
         "seed": 42,  # which seed to use
-        "env-name": "MiniHack-Quest-Easy-v0",  # name of the game
+        "env-name": "MiniHack-Quest-Hard-v0",  # name of the game
         "replay-buffer-size": int(5e3),  # replay buffer size
         "learning-rate": 1e-4,  # learning rate for Adam optimizer
         "discount-factor": 0.99,  # discount factor
@@ -404,8 +404,8 @@ if __name__ == "__main__":
         "use-double-dqn": True,  # use double deep Q-learning
         "target-update-freq": 1000,  # number of iterations between every target network update
         "eps-start": 1.0,  # e-greedy start threshold
-        "eps-end": 0.01,  # e-greedy end threshold
-        "eps-fraction": 0.1,  # fraction of num-steps
+        "eps-end": 0.1,  # e-greedy end threshold
+        "eps-fraction": 0.2,  # fraction of num-steps
         "print-freq": 25, # number of iterations between each print out
         "save-freq": 500, # number of iterations between each model save
     }
